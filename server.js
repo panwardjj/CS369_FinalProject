0cb9e1ebfb5c27c4650bbd4ed5bec51f419e3acb
panwardjj <panward.jin@dome.tu.ac.th>
2024-05-30 10:42:27 +0700

    add new server

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(express.json());
 const dbConfig = {
     user: 'SA',
     password: 'MyStrongPass123',
-    server: 'localhost',
+    server: 'store.c98cuum2qn3w.us-east-1.rds.amazonaws.com',
     database: 'store',
     options: {
         encrypt: false, 
